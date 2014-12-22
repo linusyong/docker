7c55fe3ea55abe15117f8b6e05c0e210dad383c8
Linus Yong <linusyong@gmail.com>
2014-12-22 07:27:16 +0000

    added --net=host option for running docker containers

--- nginx-php-fpm/nginx/run_docker.sh
+++ nginx-php-fpm/nginx/run_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker run \
   --name nginx \
+  --net=host \
   -v /home/core/docker/nginx-php-fpm/exports/html:/var/www/html \
   -v /home/core/docker/nginx-php-fpm/nginx/exports/etc:/etc/nginx \
   -v /home/core/docker/nginx-php-fpm/nginx/exports/log:/var/log/nginx \
--- nginx-php-fpm/php-fpm/run_docker.sh
+++ nginx-php-fpm/php-fpm/run_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker run \
   --name php-fpm \
+  --net=host \
   -v /home/core/docker/nginx-php-fpm/exports/html:/var/www/html \
   -v /home/core/docker/nginx-php-fpm/php-fpm/exports/etc:/etc/php5 \
   -p 9000:9000 -d php-fpm
